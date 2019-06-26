83ae87e9c0eb9303ec7543e5c7fe1185b8dc04ee
Eric Thiel <ethiel172@gmail.com>
2019-06-26 16:35:19 -0400

    letter check functionality added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 // ==================
 // 
-// 
+// alphabet array to compare guess against
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Game data
 var chances = 10;
 var wins = 0;
 var losses = 0;
@@ -12,22 +14,25 @@ console.log("I am thinking of the letter " + "`" + computerGuess + "` and you're
 document.onkeyup = function(event){
   var guess =  event.key.toLocaleLowerCase();  
   console.log("User Guess: " + guess);
-  
+  var letterCheck = alphabet.indexOf(guess)
+  // Checking the key pressed exists in the alphabet array
+ 
   // Win Condition
   if (guess === computerGuess){
     console.log(true);
     wins++;
     console.log("Wins: " + wins);
   }
-  // Checking the key pressed exists in the alphabet array
-  else if (guess = "1"){
-    console.log("WRONG")
+  // Check key pressed is a letter
+  else if (letterCheck === -1){
+    console.log("Only guess letters please.")
+
   }
   //
   else{
     chances = chances - 1;
-    console.log(chances);
-    if (chances <= 0){
+    console.log("Incorrect. Remaining chances: " + chances);
+    if (chances === 0){
       alert("YOU LOSE");
       losses++;
       console.log("Losses: " + losses);
