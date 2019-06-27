b09371519b78f58faab8e42486efd9ab1aa97951
Eric Thiel <ethiel172@gmail.com>
2019-06-27 10:21:54 -0400

    updates HTML with game data each key press

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,16 @@ var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 var chances = 10;
 var wins = 0;
 var losses = 0;
-console.log("Guesses Left: " + chances);
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 console.log("I am thinking of the letter " + "`" + computerGuess + "` and you're a cheater");
+
+
+var directionsText = document.getElementById("instructions");
+var userChoiceText = document.getElementById("userGuess");
+var resultText = document.getElementById("guessResult");
+var chancesText = document.getElementById("chances");
+var winsText = document.getElementById("wins");
+var lossesText = document.getElementById("losses");
 
 document.onkeyup = function(event){
   var guess =  event.key.toLocaleLowerCase();  
@@ -19,23 +26,43 @@ document.onkeyup = function(event){
  
   // Win Condition
   if (guess === computerGuess){
-    console.log(true);
+    
     wins++;
-    console.log("Wins: " + wins);
+    userChoiceText.textContent = "Your Guess: " + guess;
+    resultText.textContent = "Result: CORRECT!"
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    alert("YOU WIN! Press OK to play again!");
+
+    // Reset the game after a win
+    chances = 10
+    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log("I am thinking of the letter " + "`" + computerGuess + "` and you're a cheater");
+    
   }
   // Check key pressed is a letter
   else if (letterCheck === -1){
-    console.log("Only guess letters please.")
+    alert("Only guess letters please.")
 
   }
   //
   else{
     chances = chances - 1;
-    console.log("Incorrect. Remaining chances: " + chances);
+    userChoiceText.textContent = "Your Guess: " + guess;
+    resultText.textContent = "Result: WRONG!"
+    chancesText.textContent = "Remaining Guesses: " + chances;
     if (chances === 0){
-      alert("YOU LOSE");
+      alert("YOU LOSE! PRESS OK TO PLAY AGAIN");
       losses++;
-      console.log("Losses: " + losses);
+      resultText.textContent = "Result: YOU LOST!";
+      winsText.textContent = "Wins: " + wins;
+      lossesText.textContent = "Losses: " + losses;
+
+      // Reset the game after a loss
+      computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+      console.log("I am thinking of the letter " + "`" + computerGuess + "` and you're a cheater");
+      chances = 10
+      chancesText.textContent = "Remaining Guesses: " + chances;
     }
   }
 }
